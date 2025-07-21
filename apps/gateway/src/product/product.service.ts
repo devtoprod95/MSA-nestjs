d526b02f1aa3b284ef212082c1cda01a1aa97b88
junyeob.kim <junyeob.kim@onch3.co.kr>
2025-07-21 15:26:47 +0900

    feat: gateway product 작업

--- apps/gateway/src/product/product.service.ts
+++ apps/gateway/src/product/product.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { PRODUCT_SERVICE } from '@app/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    constructor(
+        @Inject(PRODUCT_SERVICE)
+        private readonly productMicroService: ClientProxy,
+    ){}
+
+    async createSamples(){
+        return lastValueFrom(this.productMicroService.send({ cmd: 'create_samples'}, {}));
+    }
+}
--- apps/product/product/product.controller.ts
+++ apps/product/product/product.controller.ts
@@ -8,7 +8,9 @@ import { RpcInterceptor } from '@app/common/interceptor/rpc.interceptor';
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Post('sample')
+  @MessagePattern({ cmd: 'create_samples' })
+  @UsePipes(ValidationPipe)
+  @UseInterceptors(RpcInterceptor)
   createSamples(){
     return this.productService.createSamples();
   }
