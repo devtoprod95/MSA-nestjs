a41a5fb4417b92be53344f73ad0196d61334af29
dohan.kim <junyeob.kim@onch3.co.kr>
2026-03-04 10:17:10 +0900

    feat: grpc productMicroservice 작업

--- apps/product/product/product.controller.ts
+++ apps/product/product/product.controller.ts
@@ -1,25 +1,27 @@
-import { Controller, Get, Post, UseInterceptors, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { ProductService } from './product.service';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { Payload } from '@nestjs/microservices';
 import { GetProductsInfo } from './dto/get-products-info.dto';
-import { RpcInterceptor } from '@app/common/interceptor/rpc.interceptor';
+import { ProductMicroservice } from '@app/common';
 
 @Controller('product')
-export class ProductController {
+export class ProductController implements ProductMicroservice.ProductServiceController {
   constructor(private readonly productService: ProductService) {}
 
-  @MessagePattern({ cmd: 'create_samples' })
-  @UsePipes(ValidationPipe)
-  @UseInterceptors(RpcInterceptor)
-  createSamples(){
-    return this.productService.createSamples();
+  async createSamples(){
+    const resp = await this.productService.createSamples();
+    
+    return {
+      success: resp
+    }
   }
 
-  @MessagePattern({ cmd: 'get_products_info' })
-  @UsePipes(ValidationPipe)
-  @UseInterceptors(RpcInterceptor)
-  getProductInfo(@Payload() data: GetProductsInfo){
-    return this.productService.getProductsInfo(data.productIds);
+  async getProductInfo(@Payload() data: GetProductsInfo){
+    const resp = await this.productService.getProductsInfo(data.productIds);
+
+    return {
+      products: resp
+    }
   }
 
 }
